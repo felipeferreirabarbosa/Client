ebc4ec7c20cfe07c2f7632c2dc93f3e8e934841c
felipe.ferreira <felipe.ferreira@esig.com.br>
2020-10-26 22:41:54 -0300

    First Commit

---
+++ src/main/java/io/github/felipeferreirabarbosa/client/ClientsApplication.java
@@ -0,0 +1,11 @@
+package io.github.felipeferreirabarbosa.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientsApplication.class,args);
+    }
+}
